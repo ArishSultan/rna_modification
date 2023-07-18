1bd47bf1c01af79d3e8316ca26ebb1a6dd1e0057
Arish Khan <arishkhan28.08@gmail.com>
2023-07-18 14:14:46 +0500

    added 2ome modification dataset

---
+++ main.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+
+
+def transform_to_csv(in_path: Path, out_path):
+    data = []
+    with open(in_path) as file:
+        lines = file.readlines()
+
+        for i in range(0, len(lines), 2):
+            label = 1 if 'P' in lines[i].split('|')[0] else 0
+            seq = lines[i + 1].strip()
+
+            data.append((label, seq))
+
+    with open(out_path, 'w') as file:
+        for item in data:
+            file.write(f'{item[0]},{item[1]}\n')
+
+
+transform_to_csv(
+    '/Users/arish/Workspace/research/rna_modification/data/raw/2ome/training/h.sapiens.csv',
+    '/Users/arish/Workspace/research/rna_modification/data/raw/2ome/training/h.sapiens.a.csv',
+)
+
+transform_to_csv(
+    '/Users/arish/Workspace/research/rna_modification/data/raw/2ome/training/m.musculus.csv',
+    '/Users/arish/Workspace/research/rna_modification/data/raw/2ome/training/m.musculus.a.csv',
+)
+
+transform_to_csv(
+    '/Users/arish/Workspace/research/rna_modification/data/raw/2ome/training/s.cerevisiae.csv',
+    '/Users/arish/Workspace/research/rna_modification/data/raw/2ome/training/s.cerevisiae.a.csv',
+)
