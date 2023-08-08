ac272844c32e23adfc261073a90c39723a9ac675
Arish Khan <arishkhan28.08@gmail.com>
2023-08-08 14:04:32 +0500

    changed the experiment api.

--- src/experiment/experiment.py
+++ src/experiment/experiment.py
@@ -8,7 +8,7 @@ from ..data.seq_bunch import SeqBunch
 
 
 class Experiment:
-    def __init__(self, factory: ModelFactory, test: SeqBunch | None, train: SeqBunch, encoding: TransformerMixin, k=5):
+    def __init__(self, factory: ModelFactory, test: SeqBunch | None, train: SeqBunch, encoding, k=5):
         self.k = k
         self.test = test
         self.train = train
@@ -18,8 +18,9 @@ class Experiment:
     def run(self):
         k_fold = KFold(n_splits=self.k, random_state=42, shuffle=True)
 
-        y = self.train.targets
-        x = self.encoding.fit_transform(self.train.samples)
+        encoded_train = self.encoding.fit_transform(self.train)
+        y = encoded_train.targets
+        x = encoded_train.samples
 
         k_fold_reports = []
         for train_index, test_index in k_fold.split(x):
@@ -36,8 +37,9 @@ class Experiment:
                 'train': k_fold_reports
             }
 
-        test_y = self.test.targets
-        test_x = self.encoding.fit_transform(self.test.samples)
+        encoded_test = self.encoding.transform()
+        test_y = encoded_test.targets
+        test_x = encoded_test.samples
 
         model = self.factory.create_model()
         model.fit(x, y)
