16950ed6d317d633f12f3e75166ba7d7774b5f8d
arish <arish@sparkosol.com>
2024-03-26 03:44:27 +0500

    commit

--- src/dataset/loader.py
+++ src/dataset/loader.py
@@ -1,4 +1,6 @@
-from pandas import read_csv
+from pandas import read_csv, concat
+from sklearn.model_selection import train_test_split
+
 from src.utils import get_path
 
 from . import SeqBunch, Species, Modification
@@ -13,7 +15,7 @@ def load_benchmark_dataset(species: Species, modification: Modification, indepen
 
     return SeqBunch(
         targets=data[1],
-        samples=data[0],
+        samples=data.drop([1, 2], axis=1).rename({0: 'sequence'}, axis=1),
     )
 
 
@@ -33,5 +35,19 @@ def load_dataset(species: Species, modification: Modification, filtered=None, su
 
     return SeqBunch(
         targets=data[1],
-        samples=data[0],
+        samples=data.drop([1, 2], axis=1).rename({0: 'sequence'}, axis=1),
     )
+
+
+def split_balanced(x, y, test_size=0.2):
+    p = y == 1
+    n = y == 0
+
+    p_x, p_y = x[p], y[p]
+    n_x, n_y = x[n], y[n]
+
+    train_p_x, test_p_x, train_p_y, test_p_y = train_test_split(p_x, p_y, test_size=test_size, random_state=42)
+    train_n_x, test_n_x, train_n_y, test_n_y = train_test_split(n_x, n_y, test_size=test_size, random_state=42)
+
+    return (concat([train_p_x, train_n_x]), concat([test_p_x, test_n_x]),
+            concat([train_p_y, train_n_y]), concat([test_p_y, test_n_y]))
